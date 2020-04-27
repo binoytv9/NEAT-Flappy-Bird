fe14cc2498e8e6b5242d9249cdc35054ad8a7578
Binoy T V <binoytv9@gmail.com>
2020-04-27 17:47:53 +0530

    fix for round

--- flappy_bird.py
+++ flappy_bird.py
@@ -195,8 +195,8 @@ class Pipe():
         bird_mask = bird.get_mask()
         top_mask = pygame.mask.from_surface(self.PIPE_TOP)
         bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
-        top_offset = (self.x - bird.x, self.top - round(bird.y))
-        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+        top_offset = (self.x - bird.x, self.top - int(round(bird.y)))
+        bottom_offset = (self.x - bird.x, self.bottom - int(round(bird.y)))
 
         b_point = bird_mask.overlap(bottom_mask, bottom_offset)
         t_point = bird_mask.overlap(top_mask,top_offset)
